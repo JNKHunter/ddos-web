b3f726010ee957c1cd30cb2cdee161d4e5be3507
John Hunter <john@pushagency.io>
2017-06-20 18:48:43 -0400

    bootstrap

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const redis = require('redis');
+
+//Create redis client
+const redis = redis.createClient();
+client.on('connect', function() {
+    console.log("connected to redis");
+});
+
+//Set port
+const port = 3000;
+
+const app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(methodOverride('_method'));
+
+app.get('/', function(req, res, next) {
+  res.render('showhosts')
+});
+
+app.listen(port, function(){
+  console.log('Server started on port ' + port);
+});
